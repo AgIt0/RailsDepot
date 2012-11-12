c874d0ccd73f2df0378540f0d29c2afaa9dbb3b0
AgIt0 <agito82@gmail.com>
2012-11-12 17:08:45 +0200

    Improved cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,8 @@ class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
     @Time = Time.now.strftime('%I:%M %p')
+    @counter = session[:counter]
+    @counter.nil? ? @counter = 1 : @counter += 1
+    session[:counter] = @counter
   end
 end
