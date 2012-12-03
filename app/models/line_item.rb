1b6fe83efae32da9de1e336f0f154f637d0cd73d
AgIt0 <agito82@gmail.com>
2012-12-03 17:11:07 +0200

    Added auto mail

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,8 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :product, :product_id, :id, :updated_at, :cart_id, :created_at, :quantity
+  attr_accessible :product, :product_id, :id, :updated_at, :cart_id, :created_at, :quantity, :cart
   belongs_to :product
   belongs_to :cart
+  belongs_to :order
 
   def total_price
     product.price * quantity
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ActiveRecord::Base
   attr_accessible :image_url, :description, :price, :title
   default_scope :order => 'title'
   has_many :line_items
+  has_many :orders, :through => :line_items
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
